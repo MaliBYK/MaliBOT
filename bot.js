68837e0da1cfa9b215f43a2ddbd154d91295fa45
MaliBYK <developerbox00@gmail.com>
2021-03-01 07:28:55 +0300

    procfile added

--- bot.js
+++ bot.js
@@ -7,10 +7,15 @@ const client = new Discord.Client({
 });
 
 const BOT_PREFIX = "ma!";
-const MALI_KING_COMMAND = "mali kraldır";
-const I_AM_BOY_COMMAND = "bay";
-const I_AM_GIRL_COMMAND = "bayan";
+const MALI_KING_COMMAND = "mali king";
+const I_AM_BOY_COMMAND = "boy";
+const I_AM_GIRL_COMMAND = "girl";
 const SHOW_AVATAR_COMMAND = "avatar";
+const HELP_COMMAND = "help";
+const FUN_HELP_COMMAND = `${HELP_COMMAND} fun`;
+const MODERATOR_HELP_COMMAND = `${HELP_COMMAND} moderator`;
+const MEMBER_HELP_COMMAND = `${HELP_COMMAND} member`;
+const SERVER_INFO_COMMAND = "members";
 
 client.on("ready", () => {
   console.log("Bot logged In");
@@ -20,11 +25,14 @@ client.on("ready", () => {
 client.on("message", msg => {
   msg.content = msg.content.toLowerCase();
 
-  if (msg.content === `${BOT_PREFIX}${MALI_KING_COMMAND}`) reactWithCrown(msg);
+  if (msg.content.startsWith(`${MALI_KING_COMMAND}`)) reactWithCrown(msg);
   else if (msg.content === `${BOT_PREFIX}${I_AM_BOY_COMMAND}`) IAmBoy(msg);
   else if (msg.content === `${BOT_PREFIX}${I_AM_GIRL_COMMAND}`) IAmGirl(msg);
   else if (msg.content === `${BOT_PREFIX}${SHOW_AVATAR_COMMAND}`)
     ShowAvatar(msg);
+  else if (msg.content === `${BOT_PREFIX}${SERVER_INFO_COMMAND}`)
+    NumberOfPeople(msg);
+  else if (msg.content.startsWith(`${BOT_PREFIX}${HELP_COMMAND}`)) Help(msg);
 });
 
 //!FUNCTIONS START
@@ -34,17 +42,48 @@ function reactWithCrown(msg) {
 
 function IAmBoy(msg) {
   msg.member.roles.add("797567992007360522");
-  msg.reply("'Baylar' Rolü Başarıyla Eklendi!");
+  msg.reply("Role 'Baylar' Succesfully Added!");
 }
 
 function IAmGirl(msg) {
   msg.member.roles.add("797567729879482440");
-  msg.reply("'Bayanlar' Rolü Başarıyla Eklendi!");
+  msg.reply("Role 'Bayanlar' Succesfully Added!");
 }
 
 function ShowAvatar(msg) {
   msg.channel.send(msg.author.displayAvatarURL());
 }
+
+function Help(msg) {
+  const webSite = "https://www.google.com";
+  if (msg.content === "") {
+  }
+  const helpEmbed = new Discord.MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("HELP MENU")
+    .setURL(webSite)
+    // .setAuthor("MaliBOT", "https://i.imgur.com/9Pc3CWS.png", webSite)
+    .addFields(
+      {
+        name: "Fun Commands Help",
+        value: `${BOT_PREFIX}${FUN_HELP_COMMAND}`,
+      },
+      {
+        name: "Moderator Commands Help",
+        value: `${BOT_PREFIX}${MODERATOR_HELP_COMMAND}`,
+      },
+      {
+        name: "Member Commands Help",
+        value: `${BOT_PREFIX}${MEMBER_HELP_COMMAND}`,
+      }
+    )
+    .setThumbnail("https://i.imgur.com/9Pc3CWS.png")
+    .setTimestamp()
+    .setFooter("Developed by Mali#2193", "https://i.imgur.com/9Pc3CWS.png");
+
+  msg.channel.send(helpEmbed);
+}
+
 //!FUNCTIONS END
 
 //*START THE BOT
